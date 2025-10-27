eb04a184f4016c10131c021fb36976181f256568
JuanGuaragnini <62672944+6uara@users.noreply.github.com>
2025-10-27 16:06:28 -0300

    audio

---
+++ Script/sound_library.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name sound_library
+
+@export var music : Array[AudioStream]
+@export var vfxbutton : AudioStream
+@export var closetab : AudioStream
+@export var opendropdown : AudioStream
+@export var closedropdown : AudioStream
